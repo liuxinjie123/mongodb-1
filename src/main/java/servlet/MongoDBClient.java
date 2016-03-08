9ec353429d97992102d397669d182919801f95b5
aids <aidsperez@yahoo.com>
2016-03-08 23:34:07 +0800

    update: with bulk insert

--- src/main/java/servlet/MongoDBClient.java
+++ src/main/java/servlet/MongoDBClient.java
@@ -1,8 +1,24 @@
-import java.util.*;
-import org.cloudfoundry.runtime.env.*;
-import java.io.*;
-import com.mongodb.*;
-import com.mongodb.util.*;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.cloudfoundry.runtime.env.CloudEnvironment;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.WriteResult;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.util.JSON;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
 
 public class MongoDBClient
 {
@@ -12,41 +28,91 @@ public class MongoDBClient
 
     }
 
-    public boolean addEntry(String jsonString) throws Exception
+    public int addEntry(String jsonString) throws Exception, ParseException
     {
-        BasicDBObject entry = (BasicDBObject)JSON.parse(jsonString);
 
-        try{
+        JSONParser parser = new JSONParser();
+        int result = 0;
+
+        try
+        {
+
+            //get service connection and initialize mongoclient
             String connURL = getServiceURI();
-
             MongoClient mongo = new MongoClient(new MongoClientURI(connURL));
+
+            //initialize database and collection
             DB db = mongo.getDB("db");
             DBCollection table = db.getCollection("books");
-            WriteResult wr = table.insert(entry);
 
-            //Returns true if the write was acknowledged.
-            return wr.wasAcknowledged();
+            Object obj = parser.parse(jsonString);
+
+            
+            //check if json is an array or not
+            if(obj.getClass().getName().matches(".*[JSONArray]")){
+
+                JSONArray objArr = (JSONArray) obj;
+                BulkWriteOperation builder = table.initializeOrderedBulkOperation();
+
+                for(int i=0; i < objArr.size();i++)
+                {
+
+                    BasicDBObject entry = (BasicDBObject) JSON.parse(objArr.get(i).toString());
+                    builder.insert(entry);
+                }
+
+                BulkWriteResult wr = builder.execute(); // bulk insert operation
+
+                //Returns true if the write was acknowledged.
+                if(wr.isAcknowledged())
+                    result = objArr.size();
+                
+            }
+            else
+            { 
+
+                BasicDBObject entry = (BasicDBObject) JSON.parse(obj.toString());
+                WriteResult wr = table.insert(entry);
+
+                //Returns true if the write was acknowledged.
+                if(wr.wasAcknowledged())
+                    result = 1; 
+            }           
+            
         }
-        catch (Exception e) 
+        catch(ParseException pe)
         {
+
+            pe.printStackTrace();
+
+        }
+        catch(Exception e)
+        {
+
             e.printStackTrace();
         }
 
-        return false;
+        return result;
     }
 
-    public List<String> getEntry() throws Exception
-    {
-        try{
-            String connURL = getServiceURI();
 
+    public List<String> getAll() throws Exception
+    {
+        try
+        {
+
+            //get service connection and initialize mongoclient
+            String connURL = getServiceURI();
             MongoClient mongo = new MongoClient(new MongoClientURI(connURL));
+
             DB db = mongo.getDB("db");
             DBCollection table = db.getCollection("books");
 
+            //get all entries
             DBCursor cursor = table.find();
 
             List<String> entries = new ArrayList<String>();
+
             while (cursor.hasNext()) 
             {
                 entries.add(cursor.next().toString());
@@ -64,15 +130,17 @@ public class MongoDBClient
 
     public void deleteAll() throws Exception
     {
-         try{
+         try
+         {
             String connURL = getServiceURI();
-
             MongoClient mongo = new MongoClient(new MongoClientURI(connURL));
+
             DB db = mongo.getDB("db");
             DBCollection table = db.getCollection("books");
 
             table.drop();
         }
+        
         catch (Exception e) 
         {
             e.printStackTrace();
--- src/main/java/servlet/MongoDBUpload.java
+++ src/main/java/servlet/MongoDBUpload.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,10 +19,10 @@ public class MongoDBUpload extends HttpServlet
 	private MongoDBClient db = new MongoDBClient();
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	
-    	PrintWriter out = response.getWriter();
+
     	String jsonString = "";
-    	boolean addStat = false;
+    	int result = 0;
+
     	try {
 			List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
 			for (FileItem item : items) {
@@ -31,7 +31,7 @@ public class MongoDBUpload extends HttpServlet
 					// item is the file (and not a field), read it in and add to List
 					Scanner scanner = new Scanner(new InputStreamReader(item.getInputStream(), "UTF-8"));
 
-					//converting uploaded file to json
+					//converting uploaded file to json string
 					while (scanner.hasNextLine()) 
 					{
 						String line = scanner.nextLine().trim();
@@ -42,13 +42,14 @@ public class MongoDBUpload extends HttpServlet
 					}
 					scanner.close();
 
-					addStat = db.addEntry(jsonString);
-					request.setAttribute("msg", "Entry Added!");
+					result = db.addEntry(jsonString);
+
+					request.setAttribute("msg", result+" Entry Added!");
 					break;
 				}
 			}
 			
-			request.setAttribute("result", db.getEntry());
+			request.setAttribute("result", db.getAll());
 			
 		} catch (Exception e) {
 			request.setAttribute("msg", e.getMessage());
